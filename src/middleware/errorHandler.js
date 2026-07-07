9066cacb52465d247b721fca23bc30eb15c315e4
olasisi <enochlouis1@gmail.com>
2026-07-07 14:25:53 +0100

    chore: add error handler middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,8 @@
+// src/middleware/errorHandler.js
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({ error: err.message || 'Internal Server Error' });
+}
+
+module.exports = { errorHandler };
